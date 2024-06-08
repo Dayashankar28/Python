d57548a03463ea9104c1c6bd00895fb9e03e4520
Dayashankar28 <dayamm28@gmail.com>
2024-06-08 21:50:33 +0530

    dfgdfg

---
+++ Lists.py
@@ -0,0 +1,12 @@
+#What is a List?
+'''A list is a fundamental data structure in programming that allows you to store a collection of items. 
+Lists are ordered and can contain elements of various data types, such as numbers, strings, and objects.'''
+
+#creating List
+my_list = [1, 2, 3, 'apple', 'banana']
+
+#List Indexing
+''' List elements are indexed, starting from 0 for the first element. 
+You can access elements by their index. '''
+my_list = [1, 2, 3, 'apple', 'banana']
+print(my_list[4])
