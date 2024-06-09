14440c9f32f4824b3e3c1a60675f54817f9405a6
Dayashankar28 <dayamm28@gmail.com>
2024-06-09 15:59:39 +0530

    sadsadsad

--- Lists.py
+++ Lists.py
@@ -23,7 +23,7 @@ my_list = [1, 2, 3, 'apple', 'banana']
 print(my_list[-2][-2])   # it will fetch apple and within apple L
 
 # Changeable
-''' The list is changeable, meaning that we can change, add, and remove items in a list after it has been created. ''''
+''' The list is changeable, meaning that we can change, add, and remove items in a list after it has been created. '''
 
 #List Length
 my_list = [1, 2, 3, 'apple', 'banana']
@@ -117,3 +117,79 @@ thislist = ["apple", "banana", "cherry", "mango", "grapes", "berries"]
 print(len(thislist))
 thislist.clear()
 print(thislist, len(thislist))
+
+#Sort List Alphanumerically
+'''List objects have a sort() method that will sort the list alphanumerically, ascending, by default '''
+
+thislist = ["apple", "banana", "cherry", "mango", "grapes", "berries"]
+thislist.sort()
+print(thislist)
+
+thislist = ["100", "300", "600", "150", "1", "50"]
+thislist.sort()
+print(thislist)
+
+        #Sort Descending
+thislist = ["apple", "banana", "cherry", "mango", "grapes", "berries"]
+thislist.sort(reverse = True)
+print(thislist)
+
+thislist = ["100", "300", "600", "150", "1", "50"]
+thislist.sort(reverse = True)
+print(thislist)
+
+        #regardless of alphabets or numbers... just to reverse list
+thislist = ["apple", "banana", "cherry", "mango", "grapes", "berries"]
+thislist.reverse()
+print(thislist)
+
+thislist = ["100", "300", "600", "150", "1", "50"]
+thislist.reverse()
+print(thislist)
+
+#Copy a List
+'''You cannot copy a list simply by typing list2 = list1, because: list2 will only be a reference to list1,
+and changes made in list1 will automatically also be made in list2.
+
+There are ways to make a copy, one way is to use the built-in List method copy() '''
+
+thislist1 = ["apple", "banana", "cherry"]
+thislist2 = ["mango", "grapes", "berries"]
+
+list = thislist1.copy()
+print(list)
+list = thislist2.copy()
+print(list)
+
+#Join Two Lists
+''' There are several ways to join, or concatenate, two or more lists in Python.
+One of the easiest ways are by using the + operator'''
+
+thislist1 = ["apple", "banana", "cherry"]
+thislist2 = ["mango", "grapes", "berries"]
+list = thislist1 + thislist2
+print(list)
+
+thislist1 = ["apple", "banana", "cherry"]
+thislist2 = ["mango", "grapes", "berries"]
+thislist1.extend(thislist2)
+print(thislist1)
+
+# List Methods
+'''
+        Method	Description
+        
+       --> append()	Adds an element at the end of the list
+       --> clear()	Removes all the elements from the list
+       --> copy()	Returns a copy of the list
+       --> count()	Returns the number of elements with the specified value
+       --> extend()	Add the elements of a list (or any iterable), to the end of the current list
+       --> index()	Returns the index of the first element with the specified value
+       --> insert()	Adds an element at the specified position
+       --> pop()	Removes the element at the specified position
+       --> remove()	Removes the item with the specified value
+       --> reverse()	Reverses the order of the list
+       --> sort()	Sorts the list
+
+        '''
+
