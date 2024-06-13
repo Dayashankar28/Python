a684d87fd4e5163a271b87b6f8623adbd41873b2
Dayashankar28 <dayamm28@gmail.com>
2024-06-13 12:32:18 +0530

    dasdasd

---
+++ while.py
@@ -0,0 +1,26 @@
+ ######## Python Loops ######
+
+''' --> while loops
+    --> for loops
+'''
+
+## The while Loop
+'''With the while loop we can execute a set of statements as long as a condition is true.'''
+i = 2
+while i <= 20:
+    print(i)
+    i += 2
+
+'''The while loop requires relevant variables to be ready, in this example we need to define an indexing variable, i, which we set to 1.
+'''
+## -----------  The break Statement -----------
+
+'''With the break statement we can stop the loop even if the while condition is true:
+'''
+
+i = 1
+while i < 100 :
+    print(i)
+    if i == 10 :
+        break
+    i+= 1
