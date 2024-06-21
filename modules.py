448b52bd3e43db7774a4f612bc545d9968c253ab
Dayashankar28 <dayamm28@gmail.com>
2024-06-21 11:55:52 +0530

    sds

---
+++ modules.py
@@ -0,0 +1,13 @@
+#What is a Module?
+'''Consider a module to be the same as a code library.
+
+A file containing a set of functions you want to include in your application.
+'''
+#Create a Module
+'''To create a module just save the code you want in a file with the file extension .py:
+
+ExampleGet your own Python Server
+Save this code in a file named mymodule.py
+'''
+def greeting(name):
+  print("Hello, " + name)
