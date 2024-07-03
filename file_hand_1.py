0328fddad76f2876222f7be1a56fe130ab9e8408
Dayashankar28 <dayamm28@gmail.com>
2024-07-03 18:44:44 +0530

    gyjk

--- file_hand_1.py
+++ file_hand_1.py
@@ -10,17 +10,36 @@
 # print((description))
 
 # #//////////////////////////////////////////
+import requests, json, wget
 
-import json, requests, wget
-data = ""
-ur = 'https://api.github.com/users/timmywheels/repos?ref=codesnippet.io'
+# API URL
+url = 'https://fakestoreapi.com/users'
 
+# Send GET request
 response = requests.get(url)
-data = response.json()
-print(type(data))
 
-def get_users(d):
-   return d["teams_url"]
+# Check if request was successful (status code 200)
+if response.status_code == 200:
+    # Parse JSON data
+    data = response.json()
+    
+    # List to store dictionaries of id, username, and phone
+    user_data_list = []
+    
+    # Iterate over each user and extract id, username, and phone
+    for user in data:
+        user_data = {
+            "id": user['id'],
+            "username": user['username'],
+            "phone": user['phone']
+        }
+        user_data_list.append(user_data)
+    
+    # Print the list of dictionaries
+    for user_data in user_data_list:
+        print(user_data)
+else:
+    print(f"Failed to retrieve data. Status code: {response.status_code}")
 
-users = list(map(get_users, json.loads(data)))
-print(users)
+with open("example.txt", "w") as f1:
+    f1.write(json.dumps(user_data_list))
